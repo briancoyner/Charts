d240e5faae138dca205babeaf62ac692ea8725f3
Brian Coyner <815223+briancoyner@users.noreply.github.com>
2022-08-23 14:33:55 -0500

    NOISSUE - more hacks until the Charts PRs are merged

--- Package.swift
+++ Package.swift
@@ -1,13 +1,11 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Charts",
     platforms: [
-          .iOS(.v12),
-          .tvOS(.v12),
-          .macOS(.v10_12),
+          .iOS(.v15)
     ],
     products: [
         .library(
